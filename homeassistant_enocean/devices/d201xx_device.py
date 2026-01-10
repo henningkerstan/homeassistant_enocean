16b794768e8f4160b81f00cb060d58722a1be913
Henning Kerstan <mail@henningkerstan.de>
2026-01-10 21:07:00 +0100

    add switching support for D2-01 family (should be light, actually - but not yet implemented)

--- homeassistant_enocean/devices/d201xx_device.py
+++ homeassistant_enocean/devices/d201xx_device.py
@@ -5,6 +5,9 @@
 
 from homeassistant_enocean.devices.device import EnOceanDevice
 from homeassistant_enocean.entity_properties import HomeAssistantEntityProperties
+from homeassistant_enocean.types import EnOceanEntityUID
+
+from enocean.protocol.packet import RadioPacket
 
 
 class EnOceanD201XXDevice(EnOceanDevice):
@@ -65,52 +68,51 @@ class EnOceanD201XXDevice(EnOceanDevice):
         if callback:
             callback(output > 0)
 
-### OUTDATED CODE BELOW ###
+    def _get_channel_from_entity_uid(self, entity_uid: EnOceanEntityUID) -> int:
+        if len(self._switch_entities) == 1:
+            return 0
+        
+        channel = 0
+        if entity_uid.startswith("switch_"):
+            try:
+                channel = int(entity_uid.split("_")[1]) - 1
+            except Exception:
+                print("EnOceanD201XXDevice._get_channel_from_entity_uid: Invalid entity_uid format for switch entity.")
+        
+        return channel
 
+    def switch_turn_on(self, entity_uid: EnOceanEntityUID) -> None:
+        """Turn on the switch."""
+        channel = self._get_channel_from_entity_uid(entity_uid)
 
+        packet = RadioPacket.create(
+            rorg=self.device_type.eep.rorg,
+            rorg_func=self.device_type.eep.func,
+            rorg_type=self.device_type.eep.type,
+            command=0x01, # actuator set output
+            destination=self.enocean_id.to_bytelist(),
+            sender=self.sender_id.to_bytelist(),
+            DV=0x00, # switch to new output value
+            IO=channel,
+            OV=100, # output value on (100%)
+        )
+        self.send_packet(packet)
 
-#     def turn_on(self, **kwargs: Any) -> None:
-#         """Turn on the switch."""
-#         optional = [0x03]
-#         optional.extend(self.__enocean_entity_id.to_bytelist())
-#         optional.extend([0xFF, 0x00])
-#         # self.send_command(
-#         #     data=[0xD2, 0x01, self.channel & 0xFF, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00],
-#         #     optional=optional,
-#         #     packet_type=0x01,
-#         # )
-#         self._attr_is_on = True
+       
+    
+    def switch_turn_off(self, entity_uid: EnOceanEntityUID) -> None:
+        """Turn off the switch."""
+        channel = self._get_channel_from_entity_uid(entity_uid)
 
-#     def turn_off(self, **kwargs: Any) -> None:
-#         """Turn off the switch."""
-#         optional = [0x03]
-#         optional.extend(self.__enocean_entity_id.to_bytelist())
-#         optional.extend([0xFF, 0x00])
-#         # self.send_command(
-#         #     data=[0xD2, 0x01, self.channel & 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
-#         #     optional=optional,
-#         #     packet_type=0x01,
-#         # )
-#         self._attr_is_on = False
-
-#     def value_changed(self, packet: Packet) -> None:
-#         """Update the internal state of the switch."""
-#         if packet.data[0] == 0xA5:
-#             # power meter telegram, turn on if > 10 watts
-#             packet.parse_eep(0x12, 0x01)
-#             if packet.parsed["DT"]["raw_value"] == 1:
-#                 raw_val = packet.parsed["MR"]["raw_value"]
-#                 divisor = packet.parsed["DIV"]["raw_value"]
-#                 watts = raw_val / (10**divisor)
-#                 if watts > 1:
-#                     self._attr_is_on = True
-#                     self.schedule_update_ha_state()
-#         elif packet.data[0] == 0xD2:
-#             # actuator status telegram
-#             packet.parse_eep(0x01, 0x01)
-#             if packet.parsed["CMD"]["raw_value"] == 4:
-#                 channel = packet.parsed["IO"]["raw_value"]
-#                 output = packet.parsed["OV"]["raw_value"]
-#                 if channel == self.channel:
-#                     self._attr_is_on = output > 0
-#                     self.schedule_update_ha_state()
+        packet = RadioPacket.create(
+            rorg=self.device_type.eep.rorg,
+            rorg_func=self.device_type.eep.func,
+            rorg_type=self.device_type.eep.type,
+            command=0x01, # actuator set output
+            destination=self.enocean_id.to_bytelist(),
+            sender=self.sender_id.to_bytelist(),
+            DV=0x00, # switch to new output value
+            IO=channel,
+            OV=0, # output value off (0%)
+        )
+        self.send_packet(packet)
